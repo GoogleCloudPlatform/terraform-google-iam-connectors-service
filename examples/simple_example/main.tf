676b9dd09842eaf2979ddb3d9b1b1d810bdba5c6
Daisy <daisypkhoja@google.com>
2026-04-06 11:49:30 +0000

    changes

--- examples/simple_example/main.tf
+++ examples/simple_example/main.tf
@@ -15,7 +15,7 @@
  */
 
 module "iam_connector" {
-  source = "Daisyprakash/iam-connectors-service/google"
+  source = "../.."
 
   project_id          = var.project_id
   connector_id        = var.connector_id
